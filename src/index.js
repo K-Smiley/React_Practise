8a6c0fd9fdd55cdba6a32f66cd51a05477c36436
Kylie <ka-scott@hotmail.co.uk>
2021-01-31 17:43:13 +0000

    move 3 x const defs into array

--- src/index.js
+++ src/index.js
@@ -1,61 +1,55 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// index.js file, has App comonent to send to index.html
 const App = () => {
     const course = 'Half Stack Application Development'
-
-
-
-    const part1 = {
-      name: 'Fundamentals of React',
-        exercises: 10
-      }
-      const part2 = {
-        name: 'Using props to pass data',
-        exercises: 7
- }
-    const part3 = {
-       name: 'State of a component',
-     exercises: 14
-      }
+    const parts = [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        }
+      ]
+ 
     
+   
+      // these are the components to return in <App />
       return (
-     
-        <div>
-            <Header course={course} />
-            <Part name={part1.name}  exercises={part1.exercises}/>
-            <Part name={part2.name}  exercises={part2.exercises}/>
-            <Part name={part3.name}  exercises={part3.exercises}/>
-       
-         
-        </div>
+          <div>
+                <Header course={course} />
+                <Part parts={parts[0]} />
+                <Part parts={parts[1]} />
+                <Part parts={parts[2]} />
+         </div>
       )
-
 }
 
+// component for Header
 const Header = (props) => {
-    console.log(props)
+    // console.log(props)
     return (
-         <div>
+        <div>
             <h1>'{props.course}'</h1>
         </div>
     )
 }
-
-
-
+// component for Part
 const Part = (props) => {
-  console.log(props)
-        return (
-            <div>
-        <p>Section:  {props.name} Exercises:  {props.exercises}</p>
+    console.log(props)
+    return (
+        <div>
+           <p>Section: {props.parts.name} | No. of exercises: {props.parts.exercises}</p>
         </div>
      )
  }
-
-
-
-
 
 
 ReactDOM.render(<App />, document.getElementById('root'))
